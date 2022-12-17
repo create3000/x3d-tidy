9c9cee5b7b5bb99e65564dbb69c9c8f3c1c5dcbb
Holger Seelig <holger.seelig@gmail.com>
2022-12-17 14:50:32 +0100

    Create offscreen window.

--- bin/x3d-tidy.js
+++ bin/x3d-tidy.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
+const
+   electron = require ("electron"),
+   fs       = require ("fs"),
+   path     = require ("path")
 
-console .log ("Hello, World!")
+electron .app .whenReady () .then (() =>
+{
+   const window = new electron .BrowserWindow ({ show: false, webPreferences: { offscreen: true } })
+
+   window .loadURL ("https://github.com")
+   window .webContents .on ("paint", (event, dirty, image) =>
+   {
+      fs .writeFileSync ("ex.png", image .toPNG ())
+      console .log (`The screenshot has been successfully saved to ${path .join (process .cwd (), "ex.png")}`)
+      electron .app .quit ()
+   })
+
+   window .webContents .setFrameRate (60)
+})
