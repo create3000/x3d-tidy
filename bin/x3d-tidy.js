fb941c83ea5ddbae3a7c5642f3514efb85409118
Holger Seelig <holger.seelig@gmail.com>
2023-05-31 06:10:02 +0200

    Updated main.

--- bin/x3d-tidy.js
+++ bin/x3d-tidy.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
+"use strict"
+
 const path = require ("path")
 const { execFile } = require ("child_process")
 
 execFile ("electron", [path .resolve (__dirname, ".."), ... process .argv], (error, stdout, stderr) =>
 {
    if (error)
-   {
-      process .stderr .write (error .message);
-      return;
-   }
+      return process .stderr .write (error .message)
+
+   if (stdout)
+      process .stdout .write (stdout)
 
    if (stderr)
-      process .stderr .write (stderr);
-
-   process .stdout .write (stdout);
-});
+      process .stderr .write (stderr)
+})
