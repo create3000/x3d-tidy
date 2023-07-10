a8b8855b9864901677969cf31020db716c4db43d
Holger Seelig <holger.seelig@gmail.com>
2023-07-10 16:09:17 +0200

    Published version 1.0.49

--- bin/x3d-tidy.js
+++ bin/x3d-tidy.js
@@ -4,6 +4,8 @@
 const os = require ("os")
 const { spawn } = require ("child_process")
 
+console .log (process .pwd ())
+
 const p = spawn (os .platform () === "win32" ? "npm.cmd" : "npm", ["start", "--silent", "--", ... process .argv .slice (2)])
 
 p .stdout .pipe (process .stdout)
