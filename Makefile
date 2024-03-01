43287e3cc559f11df66ad48f73cb2824a41afec0
Holger Seelig <holger.seelig@gmail.com>
2024-03-01 12:40:02 +0100

    Removed Makefile.

--- Makefile
+++
@@ -1,3 +0,0 @@
-.PHONY: tests
-tests:
-	npx . -i tests/test.x3d -o tests/test.html
---
+++ build/test.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
+const { systemSync } = require ("shell-tools");
+
+systemSync ("npx . -i tests/test.x3d -o tests/test.html");
