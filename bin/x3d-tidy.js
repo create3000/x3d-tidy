e5a70d4a63dbf5165e8148ecdbb688ad71aec382
Holger Seelig <holger.seelig@gmail.com>
2024-10-04 12:12:27 +0200

    Run cmd in a shell.

--- bin/x3d-tidy.js
+++ bin/x3d-tidy.js
@@ -9,7 +9,7 @@ const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
 
 process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", "--cwd", cwd, ... process .argv .slice (2)]);
+const p = spawn (cmd, ["start", "--silent", "--", "--cwd", cwd, ... process .argv .slice (2)], { shell: true });
 
 p .stdout .pipe (process .stdout);
 p .stderr .pipe (process .stderr);
