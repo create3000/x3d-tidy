1d67cb66d8ce4b10d9eca77d4846e5bd1b2c58c3
Holger Seelig <holger.seelig@gmail.com>
2024-11-28 11:51:24 +0100

    Use latest x_ite-node.

--- build/test.js
+++ build/test.js
@@ -7,3 +7,4 @@ systemSync (`npx . -i tests/assets/test.x3d -o tests/out/test.html -r`);
 systemSync (`npx . -i tests/assets/test.x3d -o tests/out/test.x3d  -r -m`);
 systemSync (`npx . -i tests/assets/Primitives.svg -o tests/out/Primitives.x3d`);
 systemSync (`npx . -i https://create3000.github.io/media/examples/Geometry3D/IndexedFaceSet/IndexedFaceSet.x3d -o tests/out/https.html`);
+systemSync (`npx . -i /Volumes/Home/X3D/glTF/glTF-Sample-Models/2.0/MetalRoughSpheres/glTF-Embedded/MetalRoughSpheres.gltf -o tests//out/MetalRoughSpheres.x3d`);
