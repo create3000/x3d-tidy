39440dbd412975732acbcdf3ee2d33b996f8008e
Holger Seelig <holger.seelig@gmail.com>
2024-12-04 22:53:44 +0100

    Added abs test.

--- build/test.js
+++ build/test.js
@@ -5,6 +5,7 @@ const { systemSync } = require ("shell-tools");
 
 systemSync (`npx . -i tests/assets/test.x3d -o tests/out/test.html -r`);
 systemSync (`npx . -i tests/assets/test.x3d -o tests/out/test.x3d  -r -m`);
+systemSync (`npx . -i ${__dirname}/../tests/assets/test.x3d -o ${__dirname}/../tests/out/abs.x3d  -r -m`);
 systemSync (`npx . -i tests/assets/Primitives.svg -o tests/out/Primitives.x3d`);
 systemSync (`npx . -i https://create3000.github.io/media/examples/Geometry3D/IndexedFaceSet/IndexedFaceSet.x3d -o tests/out/https.html`);
 systemSync (`npx . -i https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/BoxTextured/glTF-Binary/BoxTextured.glb -o tests/out/BoxTextured.glb.x3d`);
