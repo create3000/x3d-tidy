c08d96a5e7eb144562e901f4d528c4b4431ed46c
Holger Seelig <holger.seelig@gmail.com>
2024-12-10 10:50:29 +0100

    Added tests.

--- build/test.js
+++ build/test.js
@@ -10,3 +10,6 @@ systemSync (`npx . -i tests/assets/Primitives.svg -o tests/out/Primitives.x3d`);
 systemSync (`npx . -i https://create3000.github.io/media/examples/Geometry3D/IndexedFaceSet/IndexedFaceSet.x3d -o tests/out/https.html`);
 systemSync (`npx . -i https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/BoxTextured/glTF-Binary/BoxTextured.glb -o tests/out/BoxTextured.glb.x3d`);
 systemSync (`npx . -i https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/BoxTextured/glTF-Embedded/BoxTextured.gltf -o tests/out/BoxTextured.embedded.x3d`);
+
+systemSync (`npx . -i tests/assets/test.x3d -o tests/out/test.o.x3d -o tests/out/test.o.x3dv -o tests/out/test.o.x3dj`);
+systemSync (`npx . tests/assets/test.x3d tests/out/test.no.x3d tests/assets/test.x3d tests/out/test.no.x3dv tests/assets/test.x3d tests/out/test.no.x3dj`);
