315505403202bcc15912be790af125394cf748e1
Holger Seelig <holger.seelig@gmail.com>
2025-11-17 11:11:09 +0100

    Added localhost server hint.

--- src/main.js
+++ src/main.js
@@ -301,7 +301,7 @@ a:hover {
     <x3d-canvas>
 ${scene .toXMLString ({ html: true, indent: " " .repeat (6) }) .trimEnd ()}
     </x3d-canvas>
-    <p>Made with <a href="https://www.npmjs.com/package/x3d-tidy" target="_blank">x3d-tidy</a>.</p>
+    <p>Made with <a href="https://www.npmjs.com/package/x3d-tidy" target="_blank">x3d-tidy</a>. If local files are not loaded <a href="https://create3000.github.io/x_ite/setup-a-localhost-server">consider setup a localhost server</a> or use <a href="https://create3000.github.io/x_ite/dom-integration">DOM integration methods</a>.</p>
   </body>
 </html>`;
 }
