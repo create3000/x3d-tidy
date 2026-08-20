0582b316fc92ced93bcf90de3de3f10c7ae2a855
Holger Seelig <holger.seelig@gmail.com>
2026-08-20 17:43:45 +0200

    Filter root nodes of type X3DMetadataObject.

--- src/metadata.js
+++ src/metadata.js
@@ -6,6 +6,8 @@ const
 
 module .exports = function metadata (scene)
 {
+   scene .rootNodes = scene .rootNodes .filter (node => !node .getNodeType () .includes (X3D .X3DConstants .X3DMetadataObject));
+
    for (const node of scene .traverse (Traverse .PROTO_DECLARATIONS | Traverse .PROTO_DECLARATION_BODY | Traverse .ROOT_NODES))
    {
       if (!(node instanceof X3D .SFNode))
